54fa6005005b348c745373466853abc9f35bfee7
moealrobiay <mohammedalrobiay12@gmail.com>
2022-09-13 15:40:58 -0600

    INIT

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-episode_name = input("Enter the Episode Name: ")
-underscore = episode_name.find("_")
+average = float(input("Enter average: "))
+
+snacks = ""
+if average <= 40:
+    snacks = "Lollipops"
+elif average <= 90:
+    snacks = "Cookies"
+else:
+    snacks = "Ice Cream"
+
+print(f"Bring {snacks}")
