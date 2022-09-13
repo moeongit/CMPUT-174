933373d269363bec7118cae329541980897e3ea6
moealrobiay <mohammedalrobiay12@gmail.com>
2022-09-13 14:46:53 -0600

    init

--- Lab_1/episode.py
+++ Lab_1/episode.py
@@ -4,6 +4,6 @@ episode_season = episode_name[1:underscore]
 episode_name = episode_name[underscore+1:]
 underscore = episode_name.find("_")
 episode_number = episode_name[1:underscore]
-episode_name = episode_name[underscore+1:]
+episode_title_name = episode_name[underscore+1:]
 
-print("Season " + str(episode_season) + ", Episode " + str(episode_number) + ": " + episode_name + " (The Simpsons)")
+print("Season " + str(episode_season) + ", Episode " + str(episode_number) + ": " + episode_title_name + " (The Simpsons)")
---
+++ test.py
@@ -0,0 +1,2 @@
+episode_name = input("Enter the Episode Name: ")
+underscore = episode_name.find("_")
