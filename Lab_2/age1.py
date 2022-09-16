58f62c729dec9c657f4aea20d48e92ee76254691
moealrobiay <mohammedalrobiay12@gmail.com>
2022-09-16 13:43:43 -0600

    commit message

--- Lab_2/age1.py
+++ Lab_2/age1.py
@@ -5,13 +5,13 @@ age = int(input("What is the character's age? ")) # Asks the user to input an ag
 frodo_age = 51 # Frodo's age is 51 so I used that to compare it to the character's ages.
 if age < 0:
     print("Please type a positive number.") # If a user types an integer less than 0, it will tell them to print a positive number.
-younger_older = "" 
-if str(age) > str(frodo_age):
-    younger_older = "older" # If they are older, the function will be "older".
-else:
-    younger_older = "younger" # Or if they are younger, the function will be "younger".
-if str(age) == str(frodo_age):
-    younger_older = "same age" # If the character is the same age as Frodo, it will print out that they are the same age.
-    print(f"{name} is {age} years old, and they are of the same age as Frodo.") # I had to make 2 print statements because they could be the same age as Frodo.
-else:
-    print(f"{name} is {age} years old, and they are {younger_older} than Frodo.")
+else: 
+    if age > frodo_age: 
+        younger_older = "older" # If they are older, the function will be "older".
+    else:
+        younger_older = "younger" # Or if they are younger, the function will be "younger".
+    if str(age) == str(frodo_age):
+        younger_older = "same age" # If the character is the same age as Frodo, it will print out that they are the same age.
+        print(f"{name} is {age} years old, and they are of the same age as Frodo.") # I had to make 2 print statements because they could be the same age as Frodo.
+    else:
+        print(f"{name} is {age} years old, and they are {younger_older} than Frodo.")
--- Lab_2/age2.py
+++ Lab_2/age2.py
@@ -1,14 +1,17 @@
 # This program takes the name and age of a character and compares it to Frodo and Gollum's age.
+from pdb import Restart
+
+
 frodo_age = 51
 gollum_age = 589
 name = input("What is the character's name? ").capitalize() # Asks the user for a character name and capitalizes the first letter of that name.
 age = int(input("What is the character's age? ")) # Asks the user for an integer of the character's age.
 if age < 0:
     print("Please type a positive number.") # If the age is less than 0, the program asks the user to type a positive number".
-younger_older = ""
-if age > frodo_age and gollum_age: 
-    print(f"{name} is {age} years old, and they are older than Gollum and Frodo.") # If the character's age is greater than Frodo's and Gollum's, it will print that out.
-elif frodo_age < age < gollum_age:
-    print(f"{name} is {age} years old, and they are older than Frodo but younger than Gollum.") #  If the character's age is greater than Frodo's but less than Gollum's, it will print that out.
-elif age < frodo_age and gollum_age:
-    print(f"{name} is {age} years old, and they are younger than Frodo and Gollum.") # If the character's age is less than Frodo's and Gollum's, it will print that out.
+else:
+    if age > frodo_age and gollum_age: 
+        print(f"{name} is {age} years old, and they are older than Gollum and Frodo.") # If the character's age is greater than Frodo's and Gollum's, it will print that out.
+    elif frodo_age < age < gollum_age:
+        print(f"{name} is {age} years old, and they are older than Frodo but younger than Gollum.") #  If the character's age is greater than Frodo's but less than Gollum's, it will print that out.
+    elif age < frodo_age and gollum_age:
+        print(f"{name} is {age} years old, and they are younger than Frodo and Gollum.") # If the character's age is less than Frodo's and Gollum's, it will print that out.
