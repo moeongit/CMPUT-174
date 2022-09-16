8caf1be3a54f80425b51e8ea0483a1f49b8f6858
moealrobiay <mohammedalrobiay12@gmail.com>
2022-09-16 13:45:23 -0600

    init

--- Lab_2/age4.py
+++ Lab_2/age4.py
@@ -1,25 +1,5 @@
-
+# This program
 name = input("What is the character's name? ")
 age = int(input("What is the character's age? "))
 if age < 0:
     print("Invalid age.")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
