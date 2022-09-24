9b1193a83237754ff3eeb5f9bd3a17229ad61632
moealrobiay <mohammedalrobiay12@gmail.com>
2022-09-24 12:04:38 -0600

    init

--- Lab_3/klingon-quiz1.py
+++ Lab_3/klingon-quiz1.py
@@ -1,10 +1,11 @@
-textFile = open("klingon-english.txt", "r")
-line1 = textFile.readline()
+# This program translates the word "Computer" in English to Klingon. It opens the text file with words translated to and from English and Klingon and reads it
+textFile = open("klingon-english.txt", "r") # Opened the text file, "r" is to read it
+line1 = textFile.readline() # We wanted line 3 so i read everyline up until line 3
 line2 = textFile.readline()
 line3 = textFile.readline()
 
-line3Almost = line3.split("|")[1]
-line3Processed = line3Almost.strip("\n")
+line3Almost = line3.split("|")[1] # This takes line 3, splits the vertical bar (|) and the [1] takes the first index after the bar, which is computer
+line3Processed = line3Almost.strip("\n") # This takes out the "\n" in line 3
 x = input(f"How do you translate {line3Processed} to Klingon?\n")
 
 if str(x) == str("De'wI'"):
