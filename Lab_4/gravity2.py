14b81fb5a48bd8a48b42b66367fa5bbe8c372ffa
malrobiay04@gmail.com <you@example.com>
2022-10-04 17:21:14 -0600

    init

--- Lab_4/gravity2.py
+++ Lab_4/gravity2.py
@@ -1,13 +1,26 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabet_lower = "abcdefghijklmnopqrstuvwxyz"
+alphabet_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def decrypt_ceaser(text, shift) :
+    decrypt = ""
+    for letter in text:
+        if 65 <= ord(letter) <= 90:
+            newletter = chr(65 + (ord(letter)-65+shift) % 26)
+            decrypt = decrypt + newletter
+        elif 97 <= ord(letter) <= 122:
+            newletter = chr(97 + (ord(letter)-97+shift) % 26)
+            decrypt = decrypt + newletter
+        else:
+            decrypt = decrypt + letter
+    return decrypt
 
 
 
+def main():
+    user = input("Enter a string: ")
+    print(decrypt_ceaser(user, -3))
 
-
-
-
-
-
+main()
 
 
 
