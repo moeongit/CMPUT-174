94e849a13143d80a7b52101dc87007f08e524fed
moealrobiay <mohammedalrobiay12@gmail.com>
2022-10-04 17:00:33 -0600

    init

--- Lab_4/gravity1.py
+++ Lab_4/gravity1.py
@@ -2,10 +2,10 @@ def decrypt_ceaser(text, shift) :
     decrypt = ""
     for letter in text:
         if 65 <= ord(letter) <= 90:
-            newletter = chr(65 + (ord(letter)-65+shift)%26)
+            newletter = chr(65 + (ord(letter)-65+shift) % 26)
             decrypt = decrypt + newletter
         elif 97 <= ord(letter) <= 122:
-            newletter = chr(97 + (ord(letter)-97+shift)%26)
+            newletter = chr(97 + (ord(letter)-97+shift) % 26)
             decrypt = decrypt + newletter
         else:
             decrypt = decrypt + letter
@@ -15,3 +15,4 @@ def main():
     print(decrypt_ceaser(user, -3))
 
 main()
+
