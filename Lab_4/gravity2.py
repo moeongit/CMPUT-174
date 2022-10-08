a03c4ef018be7038c38347e70d60a3a04970f2b9
malrobiay04@gmail.com <you@example.com>
2022-10-07 19:00:06 -0600

    Merge branch 'main' of https://github.com/moeongit/CMPUT_174

--- Lab_4/gravity2.py
+++ Lab_4/gravity2.py
@@ -11,7 +11,7 @@ def decrypt_ceaser(text, shift): # Parameters are text and shift
             decrypt = decrypt + letter
     return decrypt
 
-def decrypt_atbash(text): # Parameter is string
+def decrypt_atbash(text): # Parameter is text
     decrypt = ""
     for letter in text:
         if 65 <= ord(letter) <= 90: # Used ord and Ascii values to implement this
@@ -23,46 +23,40 @@ def decrypt_atbash(text): # Parameter is string
         else:
             decrypt = decrypt + letter
     return decrypt
-'''
-def decrypt_A1Z26(str):
-    decrypt = ""
-    dash = str.find('-')
-    while True:
-        if dash == -1:
-            letter = str
-        else:
-            letter = str[0:dash]
-            str = str[dash+1:]
-        if letter == "":
-            break
-        count = 0
-        if " " in letter:
-            for counting in letter:
-                if counting != ' ':
-                    count+=1
-                else:
-                    break
-        if count == 0:
-            decrypt += chr(65 + (int(letter) -1))
-        else:
-            letter_1 =int(letter[0:count])
-            decrypt += chr(65 + (int(letter_1) -1))
-            letter_2 = ' '
-            decrypt += letter_2
-            letter_3 = int(letter[count+1:])
-            decrypt += chr(65 + (int(letter_3) -1))
-        #print(decrypt)
-        if dash == -1:
-            break
-        dash = str.find('-')
-    return decrypt    
-'''
 
-
+def decrypt_A1Z26(str): # 
+    for letter in str:
+        if letter.isalpha(): # if letter is in alphabet
+            return str
+    decrypt = "" # list for decrypt message
+    space_remover = str.split(" ") # Split the space in between the numbers
+    specialCharacters = ['.',',','?','"',"'","#","$","%","&","*","+","-","_","!"] # Made a spec. char list
+    empty_list = []
+    for element in space_remover:
+        empty_list = element.split("-") # split the dash
+        for numbers in empty_list: 
+            expression = True
+            for character in numbers:
+                if character in specialCharacters: # If the character is a special character, it will do this
+                    specialCharacter_finder = numbers.find(character) # Finds the character
+                    decrypt += chr(65 + (int(numbers[0:specialCharacter_finder])-1))
+                    decrypt += numbers[specialCharacter_finder]
+                    expression = False
+            if expression:
+                numbers = int(numbers) 
+                decrypt += chr(65+numbers-1)
+        decrypt += ' '
+    return decrypt
 def main():
-    text = input("Enter a text to decipher: ")
+    text = input("Enter a text to decipher: ") # Print statements for version 4
     print("Let's try all the methods we have:")
     print(f"Ceaser cipher: {decrypt_ceaser(text, -3)}")
     print(f"Atbash cipher: {decrypt_atbash(text)}")
-    #print(f"A1Z26 cipher: {decrypt_A1Z26(text)}")
+    print(f"Combined: 1) Ceaser; 2) Atbash Cipher: {decrypt_atbash(decrypt_ceaser(text, -3))}")
+    print(f"Combined: 1) Atbash; 2) Ceaser Cipher: {decrypt_ceaser(decrypt_atbash(text), -3)}")
+    print(f"A1Z26 cipher: {decrypt_A1Z26(text)}")
+    print(f"Combined: 1) A1Z26; 2) Ceaser Cipher: {decrypt_ceaser(decrypt_A1Z26(text), -3)}")
+    print(f"Combined: 1) A1Z26; 2) Atbash Cipher: {decrypt_atbash(decrypt_A1Z26(text))}")
+    print(f"Combined: 1) A1Z26; 2) Atbash; 3) Ceaser Cipher: {decrypt_ceaser(decrypt_atbash(decrypt_A1Z26(text)), -3)}")
+    print(f"Combined: 1) A1Z26; 2) Ceaser; 3) Atbash Cipher: {decrypt_atbash(decrypt_ceaser(decrypt_A1Z26(text), -3))}")
 main()
