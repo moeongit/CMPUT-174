5207050f7ee49ea5b8f987de0703bd7ffa22da7f
malrobiay04@gmail.com <you@example.com>
2022-10-11 22:24:45 -0600

    init

--- Lab 5/spells4.py
+++ Lab 5/spells4.py
@@ -2,8 +2,8 @@ import random
 import time
 
 def read_spells(filename: str):
-    with open('spells.txt') as filename:
-        spells = filename.readlines()
+    with open(filename) as file:
+        spells = file.readlines()
     return spells
 
 def get_random_spell(spells: list[str]):
@@ -28,15 +28,16 @@ def play_again() -> bool:
         return False
 
 def get_user_input(spell: str) -> str:
-    #start = time.time()
+    start = time.time()
     print(f"Type the following spell: {spell}")
     user_input = input().lower()
-    #user_time = round(time.time() - start, 2)
-    #print(f"Result: {user_time} seconds (goal: {get_target_time(spell)} seconds).")
-    return user_input
+    user_time = round(time.time() - start, 2)
+    print(f"Result: {user_time} seconds (goal: {get_target_time(spell)} seconds).")
+    return user_input, user_time
 
 def get_target_time(spell: str) -> float:
-    pass
+    TTT = len(spell) * 0.3
+    return TTT
 
 def calculate_points(spell: str, user_input: str, user_time: float) -> int:
     condition = True
@@ -44,7 +45,10 @@ def calculate_points(spell: str, user_input: str, user_time: float) -> int:
     while condition:
         spells = read_spells('spells.txt')
         spell = get_random_spell(spells)
-        user_input = get_user_input(spell)
+        print(spell)
+        get_tuple = get_user_input(spell)
+        user_input = get_tuple[0]
+        user_time = get_tuple[1]
         if user_input.lower() != spell.strip("\n"):
             score -= 5
             display_feedback(spell, user_input)
@@ -55,6 +59,7 @@ def calculate_points(spell: str, user_input: str, user_time: float) -> int:
             print(f"You get 10 points! Your score is: {score}")
         condition = play_again()
     print(f"Your final score is {score}.")
+    
 
 
 def display_feedback(spell: str, user_input: str):
@@ -66,8 +71,13 @@ def display_feedback(spell: str, user_input: str):
     return user_input
 
 def main() -> None:
+    sum = read_spells('spells.txt')
+    random = get_random_spell(sum)
     display_header()
     display_instructions()
     calculate_points(str, str, float)
+    get_target_time(random)
+
+    
     
 main()
