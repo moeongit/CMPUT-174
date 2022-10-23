7c2b09c3612539c7a9fe921f8c440f7b4121356e
moealrobiay <mohammedalrobiay12@gmail.com>
2022-10-22 23:58:26 -0600

    init

---
+++ Lab 6/yahtzee1.py
@@ -0,0 +1,37 @@
+def make_roll() -> tuple:
+    """
+    Returns a tuple of five random values between 1 and 6.
+    """
+    pass
+
+
+def sum_of_given_number(roll: tuple, number: int) -> int:
+    """
+    Returns the sum of the values in the roll that match the given number.
+    """
+    pass
+
+
+def fill_upper_section(roll: tuple) -> list:
+    """
+    Returns a list of the sums of all values in the roll.
+    """
+    pass
+
+
+def display_upper_section(upper_section_scores: list) -> None:
+    """
+    Displays the upper section.
+    """
+    pass
+
+def main():
+    """
+    Main function.
+    """
+    # TODO: Roll the dice (and print as in demo)
+    # TODO: Fill the upper section
+    # TODO: Display the upper section
+
+if __name__ == "__main__":
+    main()
