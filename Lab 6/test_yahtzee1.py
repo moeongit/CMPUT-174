8aeb70f9bb158f557b7a7697251013d370d99368
moealrobiay <mohammedalrobiay12@gmail.com>
2022-10-24 09:11:14 -0600

    init

---
+++ Lab 6/test_yahtzee1.py
@@ -0,0 +1,47 @@
+from yahtzee1 import sum_of_given_number, fill_upper_section
+
+def test_sum_of_given_number_all_different():
+    """
+    Tests sum_of_given_number() with one roll
+    where all dice have different face values.
+    """
+    # roll = TODO (hardcode a roll that satisfies this test case)
+    # assert sum_of_given_number(roll, 1) == TODO
+    # assert sum_of_given_number(roll, 2) == TODO
+    # assert sum_of_given_number(roll, 3) == TODO
+    # assert sum_of_given_number(roll, 4) == TODO
+    # assert sum_of_given_number(roll, 5) == TODO
+    # assert sum_of_given_number(roll, 6) == TODO
+    # (in total, you have six assertions in this test)
+    
+    pass
+
+def test_sum_of_given_number_all_same():
+    """
+    Test sum_of_given_number() with one roll
+    where all dice have the same face value.
+    """
+    # roll = TODO (hardcode a roll that satisfies this test case)
+    # TODO: Add assertions for all six values of `number`
+    # (in total, you will have six assertions in this test)
+
+    pass
+
+def test_sum_of_given_number_some_different():
+    """
+    Test sum_of_given_number() with one roll
+    where some dice have the same face value.
+    """
+    # roll = TODO (hardcode a roll that satisfies this test case)
+    # TODO: Add assertions for all six values of `number`
+    # (in total, you will have six assertions in this test)
+
+    pass
+
+def test_fill_upper_section():
+    """
+    Test fill_upper_section() with any one roll of your choice.
+    """
+    # roll = TODO (hardcode a roll that satisfies this test case)
+    # assert fill_upper_section(roll) == TODO
+    pass
