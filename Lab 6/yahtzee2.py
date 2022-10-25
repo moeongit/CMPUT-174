cf5508449c8a59d18fc059a615cf9ef4b7da76e9
moealrobiay <mohammedalrobiay12@gmail.com>
2022-10-24 18:33:11 -0600

    init

---
+++ Lab 6/yahtzee2.py
@@ -0,0 +1,58 @@
+import random
+
+def make_roll() -> tuple:
+    numbers = []
+    for i in range(0, 5):
+        numbers.append(random.randint(1, 6))
+    numbers = tuple(numbers)
+    print(f"Rolling the dice. . . {numbers}")
+    return numbers
+
+def sum_of_given_number(roll: tuple, number: int) -> int:
+    count = 0
+    for i in range(len(roll)):
+        if number == roll[i]:
+            count += 1
+    sum = count * number
+    return sum
+
+def fill_upper_section(roll: tuple) -> list:
+    upper = []
+    for i in range(1, 7):
+        upper.append(sum_of_given_number(roll, i))
+    return upper
+
+def display_upper_section(upper_section_scores: list) -> None:
+    print(f"Aces: {upper_section_scores[0]}")
+    print(f"Twos: {upper_section_scores[1]}")
+    print(f"Threes: {upper_section_scores[2]}")
+    print(f"Fours: {upper_section_scores[3]}")
+    print(f"Fives: {upper_section_scores[4]}")
+    print(f"Sixes: {upper_section_scores[5]}")
+
+def num_of_a_kind(roll: tuple, number: int) -> int:
+    """
+    If a roll has EXACTLY `number` dice of the same face value,
+    returns the sum of all five values in the roll.
+    Otherwise, returns 0.
+    """
+    pass
+
+def yahtzee(roll: tuple) -> int:
+    """
+    Returns 50 if the roll is a Yahtzee (all dice in the roll have the same
+    face value). Otherwise, returns 0.
+    """
+    pass
+
+def main():
+    """
+    Main function.
+    """
+    # Version 1 code
+    # TODO: Calculate and display "3 of a kind" for the given roll
+    # TODO: Calculate and display "4 of a kind" for the given roll
+    # TODO: Calculate and display "Yahtzee" for the given roll
+
+if __name__ == "__main__":
+    main()
