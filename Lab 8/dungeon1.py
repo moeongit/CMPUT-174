cb3921eb8166a6d6e16c0fcdff6e27413970cdda
moeongit <malrobiay04@gmail.com>
2022-11-10 15:54:37 -0700

    init

--- Lab 8/dungeon1.py
+++ Lab 8/dungeon1.py
@@ -2,29 +2,37 @@ MAP_FILE = 'cave_map.txt'
 
 def load_map(map_file: str) -> list[list[str]]:
     grid = []
-    with open(map_file, 'r') as file:
+    with open(map_file, 'r') as file: 
         for line in file:
-            convert = list(line.strip())
-            grid.append(convert)
+            convert = list(line.strip()) # Converts to a list and strips the "\n"
+            grid.append(convert) # Appends the lists to the list grid
     print(grid)
 
 def find_start(grid: list[list[str]]) -> list[int, int]:
     """
     Finds the starting position of the player on the map.
     """
-    # TODO: implement this function
+    
+
 
 def get_command() -> str:
     """
     Gets a command from the user.
     """
-    # TODO: implement this function
+    while True:
+        user_input = input("> ")
+        if user_input.lower() == "escape":
+            exit()
+        print("I do not understand.")
 
 
 def main():
     """
     Main entry point for the game.
     """
-    load_map('cave_map.txt')
+    
+    grid = load_map('cave_map.txt')
+    # find_start(grid)
+    get_command()
 if __name__ == '__main__':
     main()
