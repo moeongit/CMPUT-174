462c30c55010b07753435078b036fab45087268c
moeongit <malrobiay04@gmail.com>
2022-11-18 20:20:27 -0700

    init

---
+++ Lab 9/shows1.py
@@ -0,0 +1,35 @@
+import requests, requests_cache
+requests_cache.install_cache('cmput174_cache')
+
+BASE_URL = "https://api.tvmaze.com/"
+
+def get_shows(query: str) -> list[dict]:
+    """
+    Search for TV shows using the TV Maze API.
+    If the show is not found, return None
+    """
+    
+    shows = requests.get(BASE_URL)
+    
+    
+
+
+def main():
+    """
+    Main function 
+    """
+    query = input("Search for a show: ")
+    results = get_shows(query)
+    if not results:
+        print("No results found")
+    else:
+        n = 1
+        print("Here are the results:")
+        for result in results:
+            show = result["show"]
+            print(f"{n}. {show['name']}")
+            n += 1 
+
+if __name__ == '__main__':
+    main()
+
