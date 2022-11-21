325783f72c09ed630f378b49ad57307aa730db79
moeongit <malrobiay04@gmail.com>
2022-11-21 13:33:21 -0700

    init

--- Lab 9/shows3.py
+++ Lab 9/shows3.py
@@ -36,14 +36,32 @@ def get_seasons(show_id: int) -> list[dict]:
     """
     new_url = f"https://api.tvmaze.com/shows/{show_id}/seasons"
     response = requests.get(new_url)
-    return response.json()
-
+    if response.status_code == 200:
+        return response.json()
 
 def format_season_name(season: dict) -> str:
     """
     Format the season name
     """
-    # TODO: Implement the function
+    if season['number'] is not None:
+        season_number = season['number']
+    else:
+        season_number = "?"
+    if season['premiereDate'] is not None:
+        season_premiere_year = season['premiereDate'].split("-")[0]
+    else:
+        season_premiere_year = "?"
+    if season['endDate'] is not None:
+        end_year = season['endDate'].split("-")[0]
+    else:
+        end_year = "?"
+    if season['episodeOrder'] is not None:
+        episodes_number = season['episodeOrder']
+    else:
+        episodes_number = "?"
+
+    return f"Season {season_number} ({season_premiere_year} - {end_year}), {episodes_number} episodes"
+    
 
 def main():
     query = input("Search for a show: ")
@@ -58,7 +76,19 @@ def main():
             show = result["show"]
             print(f"{n}. {format_show_name(show)}")
             n += 1 
-        select = input("Select a show: ")
+        select = int(input("Select a show: "))
+        print(f"Seasons of {query}:")
+        selected_show = results[select - 1]["show"]
+        show_id = selected_show["id"]
+        seasons = get_seasons(show_id)
+        counter = 1
+        if len(seasons) == 0:
+            print("no seasons found")
+        else:
+            for season in seasons:
+                results = format_season_name(season)
+                print(f"{counter}. {results}")
+                counter += 1                
         
 if __name__ == '__main__':
     main()
